99f3af3f00862a270e53f083268be4d50c85cd5a
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2021-05-21 21:26:38 +0700

    Refactor routes code

--- src/routers/UserRoutes.ts
+++ src/routers/UserRoutes.ts
@@ -1,20 +1,15 @@
-import { Router } from "express";
-import IRouter from "./RouteInterface";
+import BaseRoutes from "./BaseRoutes";
 
 // Controllers
 import UserController from "../controllers/UserController";
 
-class UserRoutes implements IRouter {
-    public router: Router;
-
-    constructor() {
-        this.router = Router();
-        this.routes();
-    }
-
+class UserRoutes extends BaseRoutes {
     public routes(): void {
         this.router.get("/", UserController.index);
         this.router.post("/", UserController.create);
+        this.router.get("/:id", UserController.show);
+        this.router.put("/:id", UserController.update);
+        this.router.delete("/:id", UserController.delete);
     }
 }
 
